f2c1bc2891548224793e50bfa554d5d6af406593
deniz-duman <eduman@stuy.edu>
2016-01-11 22:57:17 -0500

    Update, put in some attributes and methods

--- Location.java
+++ Location.java
@@ -1,3 +1,21 @@
 //class Location
+import java.util.ArrayList;
+
 public class Location {
+  private String _name, _desc;
+  private int[] _coor;
+  private ArrayList<Objects> _objects;
+  private ArrayList<Entities> _entities;
+  private boolean empty;
+  
+  public Location(){
+    boolean empty = true;
+  }
+  
+  public Location (String name, String desc, int[] coor, ArrayList<Objects> objs, ArrayList<Entities> ents) {
+    _name = name;
+    _desc = desc;
+    
+    _objects = new ArrayList<Objects>;
+ 
 }
