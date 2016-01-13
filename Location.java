289094d7f5bae644bb3efebaa110a82b0d8ad66e
seabasschin <sdittgen@stuy.edu>
2016-01-12 23:08:14 -0500

    added to constructor, built toString

--- Location.java
+++ Location.java
@@ -15,7 +15,16 @@ public class Location {
   public Location (String name, String desc, int[] coor, ArrayList<Objects> objs, ArrayList<Entities> ents) {
     _name = name;
     _desc = desc;
+    _coor = coor;
+    _objects = new ArrayList<Objects>();
+    _entities = new ArrayList<Entities>();
+  }
+  
+  public static String toString() {
+    String retStr = _name + ": " + _desc;
+    return retStr;
     
-    _objects = new ArrayList<Objects>;
- 
+  }
+  
+  
 }
