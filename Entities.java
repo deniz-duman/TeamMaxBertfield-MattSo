d4806179c982e40d3afcc80bdc874d31eefe342c
seabasschin <sdittgen@stuy.edu>
2016-01-13 15:31:16 -0500

    Update Entities.java

--- Entities.java
+++ Entities.java
@@ -1,15 +1,16 @@
 //class Entities: superclass for the players and all living things in the game.
 public interface Entities {
-  
+  protected double hp, dmg;
+  protected String name, desc;
+  protected ArrayList<Objects> inventory;
+  protected boolean isAlive;
   String getName();
   String getDesc();
-  String getDialogue();
   
-  int getHealth();
-  int setHealth(int newHealth); //should return old health
-  
-  int[] getLoc();
-  int[] setLoc(int newX int newY); //should return old location
-  
+  double getHP();
+  double setHP(double newHealth); //should return old health
+  double getDMG();
+  double setDMG(double newDMG);
+  void attack(double damage, Entities ent);
   
 }
