1239a38c6beb73732299fb0edc3b91577c5666b3
seabasschin <sdittgen@stuy.edu>
2016-01-12 23:24:46 -0500

    Update Entities.java

--- Entities.java
+++ Entities.java
@@ -1,3 +1,15 @@
 //class Entities: superclass for the players and all living things in the game.
 public interface Entities {
+  
+  String getName();
+  String getDesc();
+  String getDialogue();
+  
+  int getHealth();
+  int setHealth(int newHealth); //should return old health
+  
+  int[] getLoc();
+  int[] setLoc(int newX int newY); //should return old location
+  
+  
 }
