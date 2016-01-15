04bfdd6b2f6f641f3185096df00a876dc8c9eb74
seabasschin <sdittgen@stuy.edu>
2016-01-15 15:35:40 -0500

    Complete Accessors, Mutators, and methods

--- Location.java
+++ Location.java
@@ -7,7 +7,7 @@ public class Location {
   private ArrayList<Objects> _objects;
   private ArrayList<Entities> _entities;
   private boolean isPassable;
-  
+  //=========================================Constructors and ToString===================================================\\
   public Location(){
     boolean isPassable = false;
   }
@@ -26,6 +26,45 @@ public class Location {
     return retStr;
     
   }
-  
-  
+  //========================================Mutators=====================================================================\\
+  public void setPass(boolean boo) {
+    isPassable = boo;
+  }
+  public void setDesc(String desc) {
+    _desc = desc;
+  }
+  public Objects setObj(int i, Objects O) {
+    return _objects.set(i, O);
+
+  }
+  public Entities setEnt(int i, Entities E) {
+    return _entities.set(i, E);
+  }
+  //=========================================Accessors=================================================================\\
+  public String getName()      {return _name;}
+  public String getDesc()      {return _desc;}
+  public int[]  getCoor()      {return _coor;}
+  public Objects getObj(int i) {return _objects.get(i);}
+  public Entities getEnt(int i){return _entities.get(i);}
+  public boolean getPass()     {return isPassable;}
+
+  //========================================Methods===================================================================\\
+  //Add to Objects ArrayList
+  public void AddtoObj(Objects O) {
+    _objects.add(O);
+  }
+  //Add to Entities ArrayList
+  public void AddtoEnt(Entities E) {
+    _entities.add(E);
+  }
+  //Delete Object
+  public Objects DelObj(Objects O) {
+    _objects.remove(O);
+    return O;
+  }
+  //Delete Entities
+  public Entities DelEnt(Entities E) {
+    _entities.remove(E);
+    return E;
+  }
 }
