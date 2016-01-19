9bbf81e6904416d32684530d4d42506fdd45b019
seabasschin <sdittgen@stuy.edu>
2016-01-19 15:18:04 -0500

    Update GamePlay.java

--- GamePlay.java
+++ GamePlay.java
@@ -1,3 +1,11 @@
 //Driver Class
 public class GamePlay {
+  
+  public static String whatJustHappened(){
+    String retStr = "You have just";
+    return retStr;
+  }
+  public static void main(String[] args) {
+  
+}
 }
