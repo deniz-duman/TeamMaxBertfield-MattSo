fede570c0f2d45e021a27f6edd3e16ec1fc678fc
seabasschin <sdittgen@stuy.edu>
2016-01-19 18:20:29 -0500

    Update GamePlay.java

--- GamePlay.java
+++ GamePlay.java
@@ -12,17 +12,18 @@ public class GamePlay {
       playGame();
     }
     else if (response.equals("help")) {
-      System.out.println("Once you are on the battlefield, you will have to fend for yourself. \n
-      Some commands that might help: \n
-      attack - attacks specified enemy \n
-      search - look around at your surroundings \n
-      use <item> on <target> - uses specified item on target \n
-      go <cardinal direction> - go to your desired location \n
-      pickup <item> - pick up specified object
-      drop - drop item \n
-      
-      Use these commands for your advantage. Good luck.""
+      System.out.println("Once you are on the battlefield, you will have to fend for yourself. \n"
+      +"Some commands that might help: \n\n"
+      +"attack - attacks specified enemy \n"
+      +"search - look around at your surroundings \n"
+      +"use <item> on <target> - uses specified item on target \n"
+      +"go <cardinal direction> - go to your desired location \n"
+      +"pickup <item> - pick up specified object"
+      +"drop - drop item \n\n"
+      +"Use these commands for your advantage. Good luck.")
     }
-
+    else {
+      System.out.println("invalid command")
+    }
 }
 }
