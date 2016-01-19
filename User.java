00df8b49be1f3d539c42203dd9c0b8233237d1d7
seabasschin <sdittgen@stuy.edu>
2016-01-19 15:13:25 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,8 +1,16 @@
 //class User; subclass of Entities
 public class User implements Entities{
+  double hunger;
   public User(){
+    
     name = "DT 24601";
     desc = "You are Droptrooper 24601";
+    inventory.add(Weapon gun);
+    inventory.add(Weapon ammo);
+    isAlive = true;
+    hp = 100.0;
+    dmg = 100.0;
+    hunger = 0;
     
   }
 }
