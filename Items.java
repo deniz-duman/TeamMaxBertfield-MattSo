f8c3f6fccf0623e2a92370bb061a7163821f9107
seabasschin <sdittgen@stuy.edu>
2016-01-23 17:11:14 -0500

    Update Items.java

--- Items.java
+++ Items.java
@@ -3,13 +3,16 @@ public class Items implements Objects{
 
 	private String _type = "";
   	private String _desc = "";
+  	private ArrayList<Objects> Compatible = new ArrayList<Objects>();
 
   	public Items(String type, String desc){
     
-    	_type = type;
-    	_desc = desc;
+    		_type = type;
+    		_desc = desc;
   }
-
+	public Items(Items I) {
+		Compatible.add(I);	
+	}
 	public String getType() {
     	return _type;
   }
@@ -19,6 +22,9 @@ public class Items implements Objects{
   }
    	public String ToString() {
    		return getType();
-   	}
-
+  }
+  	public boolean isCompatible(Items I) {
+  		return if (Compatible.contains(I))
+  		}
+  	}
 }
