aa7d7a2e6fa332d3188b3c64a59f00c643d37634
seabasschin <sdittgen@stuy.edu>
2016-01-23 14:37:01 -0500

    Update Items.java

--- Items.java
+++ Items.java
@@ -1,3 +1,24 @@
 //Items; subclass of Objects
 public class Items implements Objects{
+
+	private String _type = "";
+  	private String _desc = "";
+
+  	public Items(String type, String desc){
+    
+    	_type = type;
+    	_desc = desc;
+  }
+
+	public String getType() {
+    	return _type;
+  }
+  
+  	public String getDesc() {
+    	return _desc;
+  }
+   	public String ToString() {
+   		return getType();
+   	}
+
 }
