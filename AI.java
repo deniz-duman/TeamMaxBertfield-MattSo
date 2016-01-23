c9eea083986e2d548b1bb91216d6ce0a7f22a224
deniz-duman <eduman@stuy.edu>
2016-01-23 16:42:22 -0500

    Update
    No need for specific enemy, neutral, and friendly classes.

--- AI.java
+++ AI.java
@@ -1,3 +1,48 @@
 //class AI; subclass of Entities.java
-public abstract class AI implements Entities{ 
+public class AI implements Entities{
+  private String name, desc;
+  private boolean isAlive? = true, isFriend?;
+  private double hp, dmg;
+  private ArrayList<Objects> inventory = new ArrayList<Objects>();
+  
+  public AI (String n, String d, boolean fr, double hel, double dam, Objects o) {
+    name = n;
+    desc = d;
+    isFriend? = fr;
+    hp = hel;
+    dmg = dam;
+    inventory.add(o);
+  }
+  
+  public String getName() {
+    return name;
+  }
+  
+  public String getDesc() {
+    return desc;
+  }
+  
+  public double getHP() {
+    return hp;
+  }
+  public double setHP(double newHealth) { 
+    double temp = hp;
+    hp = newHealth;
+    return temp;
+  }
+  
+  public double getDMG() {
+    return dmg;
+  }
+  
+  public double setDMG(double newDMG) {
+    double temp = dmg;
+    dmg = newDMG;
+    return dmg;
+  }
+  
+   public void attack(double damage, User u) {
+    u.setHP(u.getHP() - damage);
+  }
+  
 }
