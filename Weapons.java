5f3851336fa3c6f6e500fbe759cb9096b9101b4e
seabasschin <sdittgen@stuy.edu>
2016-01-23 14:34:33 -0500

    Update Weapons.java

--- Weapons.java
+++ Weapons.java
@@ -1,3 +1,25 @@
 //Weapons; subclass of Objects
 public class Weapons implements Objects{
+	private String _type = "";
+  	private String _desc = "";
+  	private int _dmg;
+
+  	public Weapons(String type, String desc, int dmg){
+    
+    	_type = type;
+    	_desc = desc;
+    	_dmg = dmg;
+  }
+
+	public String getType() {
+    	return _type;
+  }
+  
+  	public String getDesc() {
+    	return _desc;
+  }
+   	public String ToString() {
+   		return getType();
+   	}
+
 }
