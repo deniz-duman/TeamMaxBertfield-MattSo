1a75fe5b6f2d0d08ae4e03ea444f7fdb703a2379
deniz-duman <eduman@stuy.edu>
2016-01-23 16:44:27 -0500

    Update added isGun?

--- Weapons.java
+++ Weapons.java
@@ -3,9 +3,10 @@ public class Weapons implements Objects{
 	private String _type = "";
   	private String _desc = "";
   	private int _dmg;
+  	private boolean isGun?;
 
-  	public Weapons(String type, String desc, int dmg){
-    
+  	public Weapons(String type, String desc, int dmg, boolean gun){
+    	isGun? = gun;
     	_type = type;
     	_desc = desc;
     	_dmg = dmg;
@@ -20,6 +21,11 @@ public class Weapons implements Objects{
   }
    	public String ToString() {
    		return getType();
-   	}
+  }
+  
+ 	public boolean getIsGun?() {
+ 		return isGun?;
+ 	}
+ 	}
 
 }
