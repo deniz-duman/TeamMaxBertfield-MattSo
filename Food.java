de312ff41ebafd3c02a44aeef7ab90c4be79a721
seabasschin <sdittgen@stuy.edu>
2016-01-23 14:31:33 -0500

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,3 +1,26 @@
 //class Food; subclass of Objects
 public class Food implements Objects{
+
+	private String _type = "";
+  	private String _desc = "";
+  	private int fillinglvl;
+
+  	public Food(String type, String desc, int fill){
+    
+    	_type = type;
+    	_desc = desc;
+    	fillinglvl = fill;
+  }
+
+	public String getType() {
+    	return _type;
+  }
+  
+  	public String getDesc() {
+    	return _desc;
+  }
+   	public String ToString() {
+   		return getType();
+   	}
+
 }
