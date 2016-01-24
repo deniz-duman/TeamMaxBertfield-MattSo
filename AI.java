d78acd55f1dd4b484ff48dcdd21b750741ff79ee
deniz-duman <eduman@stuy.edu>
2016-01-24 00:33:22 -0500

    Upd8

--- AI.java
+++ AI.java
@@ -1,17 +1,22 @@
 //class AI; subclass of Entities.java
 public class AI implements Entities{
-  private String name, desc;
+  private String name, desc, info;
   public boolean isAlive? = true, isFriend?;
   private double hp, dmg;
   private ArrayList<Objects> inventory = new ArrayList<Objects>();
   
-  public AI (String n, String d, boolean fr, double hel, double dam, Objects o) {
+  public AI (String n, String d, boolean fr, double hel, double dam, Objects o, String i) {
     name = n;
     desc = d;
+    info = i;
     isFriend? = fr;
     hp = hel;
     dmg = dam;
     inventory.add(o);
+  }
+  
+  public String getInfo() {
+    return info;
   }
   
   public String getName() {
